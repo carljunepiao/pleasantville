392820a6498f149d3f1065f9c1050481e72a4d8d
alfisalvacion <jmsalvacion1@up.edu.ph>
2016-05-06 11:21:36 +0800

    modified displayuser.php for more testing

--- php/admin-php-files/DISPLAY/displayuser.php
+++ php/admin-php-files/DISPLAY/displayuser.php
@@ -11,14 +11,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Fname, Lname, Contact_No FROM users";
+$sql = "SELECT UserID, Fname, Lname, Contact_No, Username, Type FROM users";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>Name</th><th>Contact No.</th></tr>";
+    echo "<table><tr><th>UserID</th><th>First Name</th><th>Last Name</th><th>Contact No.</th><th>Username</th><th>Type</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["Fname"]."</td><td>".$row["Lname"]." ".$row["Contact_No"]."</td></tr>";
+        echo "<tr><td>".$row["UserID"]."</td><td>".$row["Fname"]."</td><td>".$row["Lname"]."</td><td>".$row["Contact_No"]."</td><td>".$row["Username"]."</td><td>".$row["Type"]."</td></tr>";
     }
     echo "</table>";
 } else {
