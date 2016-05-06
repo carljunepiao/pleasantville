db50fef72d07416dae9f3ae7c0419b0c9c1c5ccd
alfisalvacion <jmsalvacion1@up.edu.ph>
2016-05-06 12:00:18 +0800

    fixed bug in displayproduction

--- php/admin-php-files/DISPLAY/displayproduction.php
+++ php/admin-php-files/DISPLAY/displayproduction.php
@@ -11,14 +11,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT ProdNo, Title, Date, Revenue FROM production";
+$sql = "SELECT Title, Date, Revenue FROM production";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>Production Number</th><th>Title</th><th>Production Date</th><th>Revenue</th></tr>";
+    echo "<table><tr><th>Title</th><th>Production Date</th><th>Revenue</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["ProdNo"]."</td><td>".$row["Title"]."</td><td>".$row["Date"]."</td><td>".$row["Revenue"]."</td></tr>";
+        echo "<tr><td>".$row["Title"]."</td><td>".$row["Date"]."</td><td>".$row["Revenue"]."</td></tr>";
     }
     echo "</table>";
 } else {
