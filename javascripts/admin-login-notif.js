f5d29d1cbda27e5ed0b8ee30e21fbec6dc3d6153
Japheth Balane <japheth162@gmail.com>
2016-05-07 19:35:52 +0800

    Added notif file for admin login;

---
+++ javascripts/admin-login-notif.js
@@ -0,0 +1,12 @@
+function notify() {
+	var title = 'Please log in to verify existence!';
+	var body = 'Robots are prohibited.';
+	var icon = '../images/adminicon.jpg';
+	var notification = new Notification(title, {
+		body: body,
+		icon: icon
+	});
+
+	setTimeout(function() { notification.close() }, 3000);
+}
+notify();
