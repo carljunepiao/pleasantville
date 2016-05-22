0cf7875b3369f43c5989dfbd526c2319dd14eddd
ShuaKo <goastig_321@yahoo.com>
2016-05-23 00:20:10 +0800

    improved addseat and addticket

--- php/admin-php-files/EDIT/editticket.php
+++ php/admin-php-files/EDIT/editticket.php
@@ -7,24 +7,24 @@ $db= 'PleasantVille';
 
 $con = new mysqli($host,$user,$pass,$db);
 
-if($con->connect-error)
+if($con)
     echo 'connected successfully to PleasantVille database';
 
-$userName = $_POST['select-user'];
+$userName = $_POST['select-user-id'];
 $Title = $_POST['select-title'];
 $Date = $_POST['select-date'];
 $Seats = $_POST['select-seat'];
 
-$sql = "SELECT userID from users WHERE username = '$userName' limit = '1'";
-$query = mysqli_query($con,$sql);
+$sql = "SELECT userID from users WHERE username = '$userName'";
+$query = $con->query($sql);
 
 if($query)
     echo 'The user exists. ';
 else
     echo 'A problem has been encountered. ';
 
-$value = mysql_fetch_object($query);
-$userID = $value->userID;
+$row = $query->fetch_assoc();
+$userID = $row["userID"];
 
 $sql1 = "UPDATE tickets SET UserID = '$userID' WHERE Title = '$Title' AND Date = '$Date' AND SeatNo = '$Seats' ";
 //UPDATE Customers SET ContactName='Alfred Schmidt', City='Hamburg' WHERE CustomerName='Alfreds Futterkiste';
@@ -36,7 +36,7 @@ else
     echo 'A problem has been encountered. ';
 
 $sql2 = "UPDATE seats SET Taken = '1' WHERE SeatNo = '$Seats' ";
-$query2 = mysqli_query($con,2);
+$query2 = mysqli_query($con,$sql2 );
 
 if($query2)
     echo 'Your seat has been reserved. ';
