b3d15548a42bd5e5fdcdcd6ed0d5d4604a767a2e
Pooween Sarana <p_louie_s@yahoo.com>
2016-05-25 22:35:03 +0800

    restore and backup

--- php/tests/restore.php
+++ php/tests/restore.php
@@ -15,7 +15,22 @@
             <center><img class="logo" src="../../images/adminicon.jpg" alt="PleasantvilleLogo"></center>
         </header>
 
-        <!--  -->
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "Pleasantville";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            shell_exec("mysql -u root pleasantville < backupss.sql 2>&1");
+            $conn->close();
+        ?>
 
     </div>
 	
