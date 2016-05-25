eb5319a6453fc062fa044cd8324cf6931a163146
Japheth Balane <japheth162@gmail.com>
2016-05-25 19:40:29 +0800

    Minor editing in home format;

--- html/index.php
+++ html/index.php
@@ -61,19 +61,14 @@
                 	echo "<div class=\"highlight\">
  	               			<h3>
  	               			<div class=\"description\">
- 	               				<img class=\"poster\" src='data:poster/jpeg;base64,". base64_encode( $row['Poster'] )."'/>
+ 	      	<img class=\"poster\" src='data:poster/jpeg;base64,". base64_encode($row['Poster'] )."'/>
  	               				<p>"
  	               					.$row["Title"]."</br>"
  	               					.$row["Playwright"]."</br>"
  	               					.$row["Time"].
  	               				"</p>
  	               				<p class=\"desc\">
- 	               					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ 	               					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse	cillum dolore eu ugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
  	               				</p>
  	               			</div>
  	               		  </div>";
